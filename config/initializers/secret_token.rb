bda6c24405230894a5f6730ee79871c0911d755c
Paul Wilson <paulanthonywilson@gmail.com>
2012-05-26 11:29:48 +0100

    Created new application using https://raw.github.com/edgecase/ecrails_templates/master/rails_templates/ecuk_standard_app.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NhsPatientlist::Application.config.secret_token = '5b9eae128eaf7c5905a16005b0479088364e75e83e153fd7f1098851e9dfda760034d915db54d8ac0eda1b878554de09b9607356469ee7e06c1d6dd60ec31315'
