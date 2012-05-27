2b410bb7efe8cf821e5ec4ba3098517eab96ca6e
Adrian Mowat <adrian.mowat@gmail.com>
2012-05-27 10:30:13 +0100

    unused code and specs are gone

--- app/controllers/admissions_controller.rb
+++
@@ -1,5 +0,0 @@
-class AdmissionsController < ApplicationController
-  def index
-    render :text=>"There are #{Admission.count} admissions"
-  end
-end
--- spec/models/to_do_item_event_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe ToDoItemEvent do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
