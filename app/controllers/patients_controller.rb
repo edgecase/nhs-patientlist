bad83e0f307d5abe7bbb647d1f798e022d9e4d80
Paul Henrich <paul@henrich.me>
2012-06-13 11:15:54 -0400

    Persist risk level

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -6,4 +6,18 @@ class PatientsController < ApplicationController
   def history
 
   end
+
+  def update_risk_level
+    @patient = Patient.find(params[:id])
+    result = (@patient.risk_level = params[:risk_level])
+    respond_to do |format|
+      if result
+        format.json { render status: 200 }
+        format.html { redirect_to :back, :notice => "Risk level updated"}
+      else
+        format.json { render status: :unproccessable_entity }
+        format.html { redirect_to :back, :notice => result.errors.full_messages }
+      end
+    end
+  end
 end
