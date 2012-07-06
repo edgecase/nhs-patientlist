dc467e1c806c8a06226481998f2209c2b4a2c450
Paul Henrich <paul@henrich.me>
2012-07-06 10:21:01 -0400

    be more clear about what we're exposing in PatientListsController

--- app/controllers/patient_lists_controller.rb
+++ app/controllers/patient_lists_controller.rb
@@ -1,23 +1,17 @@
 class PatientListsController < ApplicationController
 
+  # user's own lists (create and update)
+  expose(:own_patient_lists) { current_user.patient_lists }
+  expose(:own_patient_list)
+
+  # other users' public lists
   expose(:user)
   expose(:user_patient_lists) { user.patient_lists }
   expose(:user_patient_list)
-  expose(:patient_lists) { current_user.patient_lists }
-  expose(:patient_list)
-
-  def index
-  end
-
-  def new
-  end
-
-  def show
-  end
   
   
   def create
-    if patient_list.save
+    if own_patient_list.save
       redirect_to :action => :index, :notice => "Successfully created new list"
     else
       render :new
--- spec/controllers/patient_lists_controller_spec.rb
+++ spec/controllers/patient_lists_controller_spec.rb
@@ -7,25 +7,25 @@ describe PatientListsController do
     let(:list) { PatientList.make!(:user => current_user, :name => 'Inpatients') }
     it "gets the current user's custom patient lists" do
       get :index, { :user_id => current_user.id }
-      controller.send(:patient_lists).should eq(PatientList.find_all_by_user_id(current_user.id))
+      controller.send(:own_patient_lists).should eq(PatientList.find_all_by_user_id(current_user.id))
     end
   end
   describe '#create' do
     let(:outpatients) { "Outpatients" }
     it "creates a new custom patient list and redirects to the index" do
-      post :create, :patient_list => { "name" => outpatients}, :user_id => current_user.id
+      post :create, :own_patient_list => { "name" => outpatients}, :user_id => current_user.id
       current_user.patient_lists.find_by_name(outpatients).should_not be_nil
       response.should redirect_to(:controller => :patient_lists, :action => :index, :notice=> 'Successfully created new list')
     end
     it "handles not creating an invalid patient list" do
-      post :create, :patient_list => {}
+      post :create, :own_patient_list => {}
       response.should render_template(:new)
     end
   end
   describe '#new' do
     it "has a new page with a patient_list" do
       get :new
-      assigns(:patient_list)
+      assigns(:own_patient_list)
     end
   end
 end
