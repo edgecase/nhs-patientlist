2286c55f2e1e4c540d6bb00c0a3fbeb54284cb36
Paul Henrich <paul@henrich.me>
2012-08-01 14:37:57 -0400

    fix list creation/ list sidebar conflict
    patient_lists in the sidebar was iterating over all lists, persisted or
    not & trying to link to 'show' for each. On the new page, this is
    problematic. the solution was to explicitly ask for persisted lists.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
 
-  expose(:patient_lists) { current_user.patient_lists }
+  # patient_lists for sidebar explicitly exposes only the persisted records
+  # to prevent problem trying to link_to nonexistent list in 'new'
+  expose(:patient_lists) { current_user.patient_lists.select { |l| l.persisted? } }
+
 end
