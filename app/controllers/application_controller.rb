9f5571e502d77293d74ca3f5ee73f0cb722ae44b
Paul Henrich <paul@henrich.me>
2012-08-01 09:01:52 -0400

    add 'new list' to sidebar, continue trouble shooting list creation conflict with lists in sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
 
-  expose(:own_patient_lists) { current_user.patient_lists }
-  expose(:own_patient_list)
+  expose(:patient_lists) { current_user.patient_lists }
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe ApplicationController do
   login_user
   it "exposes the user's patient lists" do
-    controller.send(:own_patient_lists).should eq(PatientList.find_all_by_user_id(current_user.id))
+    controller.send(:patient_lists).should eq(PatientList.find_all_by_user_id(current_user.id))
   end
 end
