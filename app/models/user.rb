a389200213e2900a58ac65512d7e702e1baeff57
Adrian Mowat <adrian.mowat@gmail.com>
2012-08-21 13:51:21 +0100

    Generated devise views and set user grade on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :grade_id
   # attr_accessible :title, :body
 
 end
