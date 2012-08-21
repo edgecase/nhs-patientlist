d75b01348ee5e26ce5ca4c533c6ef9f81ae24095
Adrian Mowat <adrian.mowat@gmail.com>
2012-08-21 14:05:47 +0100

    drove grade select using association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :patient_lists
   belongs_to  :grade
 
+  validates_presence_of :grade
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
