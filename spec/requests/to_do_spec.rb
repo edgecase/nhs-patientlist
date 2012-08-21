c61a56480ac14ffd1d7b0ae5753ee12d89f55ad6
Adrian Mowat <adrian.mowat@gmail.com>
2012-08-21 15:33:01 +0100

    Tidied up spec

--- spec/requests/to_do_spec.rb
+++ spec/requests/to_do_spec.rb
@@ -3,8 +3,18 @@ require 'spec_helper'
 describe "Todos", :js => true do
   let(:user)       { User.make!(email: "test@example.com") }
   let(:other_user) { User.make! }
-  let(:patient)    { Patient.make!(:hospno=>"4567", :firstnames=>"Rita", :lastname=>"O'Really", :allergies=>"toes", :pastmedhx=>"Grouts", :id=>123)}
-  let(:admission)  { Admission.make!(:currward=>'RENAL', :admstatus => "Admitted", :patient=>patient)}
+  let(:patient)    { Patient.make!(
+                                   :hospno     => "4567",
+                                   :firstnames => "Rita",
+                                   :lastname   => "O'Really",
+                                   :allergies  => "toes",
+                                   :pastmedhx  => "Grouts",
+                                   :id         => 123)}
+  let(:admission)  { Admission.make!(
+                                     :currward=>'RENAL',
+                                     :admstatus => "Admitted",
+                                     :patient=>patient
+                                     )}
 
   before do
     patient.save
@@ -20,14 +30,15 @@ describe "Todos", :js => true do
       click_button "Add"
       find("#todo").should have_content('Check pulse')
     end
+    
     it "can be created as 'pending'" do
       fill_in 'Description', :with => 'Check bp'
       select("Pending", from: "to_do_item_status")
       click_button "Add"
       find("#pending").should have_content('Check bp')
     end
-
   end
+
   describe "an existing Todo" do
     before do
       todo = patient.to_do_items.create(description: 'starts as todo')
@@ -41,6 +52,7 @@ describe "Todos", :js => true do
       end
       find("#pending").should have_content('starts as todo')
     end
+
     it "can be marked as done" do
       within("#todo li") do
         page.should have_content('starts as todo')
@@ -48,18 +60,20 @@ describe "Todos", :js => true do
       end
       find("#done").should have_content('starts as todo')
     end
+
     it "leaves an audit trail when created" do
       visit history_patient_path(patient.id)
       find("table").should have_content('@example.com')
     end
+
     it "indicates it was created by the current user" do
       page.should have_css '.mine',  text: 'starts as todo'
     end
+
     it "omits the css indicator when it wasn't created by the current user" do
       current_user = other_user
       other_todo = patient.to_do_items.create(description: 'other todo')
       page.should_not have_css '.mine', text: 'other todo'
     end
-
   end
 end
