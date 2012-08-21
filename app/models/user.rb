61a4b36d5b5e0ee42f7d82a4f99e4be8f5f6c9e9
Adrian Mowat <adrian.mowat@gmail.com>
2012-08-21 14:06:51 +0100

    tidy up model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :grade_id
-  # attr_accessible :title, :body
-
 end
